788b414fd0cda12731373623ee0a66d30b312795
David Aldred <extrobe@users.noreply.github.com>
2021-07-05 22:26:35 +1000

    Tweaks

--- main.py
+++ main.py
@@ -35,14 +35,13 @@ IGNORE_DEFAULT_EPISODE_NAME = True # Ignore files where the Episode uses default
 SCRUB__AND__STRINGS = True # scrub 'and' from file name strings. Fixes & vs 'and' mismatches
 
 # If a file covers multiple episodes, but the episodes otherwise have the same (eg Part1 & Part2), this will allow these to be handled better
-# Multi episode files where the episode name is different should be handled OK now anyway, (once renamed to Sonarr standards), as both episode names
-# should exist in the file name 
-# Also, episode must be in the format SnnExx-yy)
+# Episode must be in the format SnnExx-yy)
 ALLOW_MULIT_PART_EPISODE_FILES = True
 
 def default_episode(title):
     """Check whether the episode title has generic naming applied."""
-    return (title[:7] == "Episode" and str.isnumeric(title[8:]))
+    #return (title[:7] == "episode" and str.isnumeric(title[8:]))
+    return bool(re.search('(episode[ ][0-9])',title))
 
 
 def is_multi_episode(file_string):
@@ -68,27 +67,14 @@ def process_season(series_id, series_name):
             if has_file: #If we don't already have a file associated with the episode, we won't need to check it
                 file = element_episode['episodeFile']['relativePath'] #file name we currently have for the episode
 
-                # The next thing we need to do is sompare the Titles. Files usually contain series/episode data
+                # The next thing we need to do is compare the Titles. Files usually contain series/episode data
                 # so we want to check the file name 'contains' the episode title.
                 # However... because file names can't contain certain punctuation which might exist in an 
                 # episode name, we need to strip out this punctuation. This also ensure your files can use periods 
                 # instead of spaces, and it will still match.
-                #
-                # There are two ways we can strip out this. the built in function e.alpha() keeps only alpha characters.
-                # I found this too loose, as it also removed numeric values, which I didn't want. Comment this out if you'd
-                # prefer this stricter approach. Instead Regex is easier to use and is the default choice
 
-                # LOOSE
-                #file_conv = str.lower(''.join(e for e in file if e.alpha()))
-                #title_conv = str.lower(''.join(e for e in title if e.alpha()))
-
-                # SNUG
                 file_conv = str.lower(re.sub('[^A-Za-z0-9]+', '', file))
                 title_conv = str.lower(re.sub('[^A-Za-z0-9]+', '', title))
-
-                # TIGHT (Don't recommend)
-                #file_conv = file
-                #title_conv = title
 
                 if SCRUB__AND__STRINGS:
                     file_conv = file_conv.replace('dvd','') # bit of a workaround!
@@ -105,10 +91,9 @@ def process_season(series_id, series_name):
                         title_conv = title_conv[:-1] # remove the part ID from the end of the name (eg 'Epidode Name (1)' becomes 'Episode Name')
                     #title_conv = title_conv.replace('part','') # remove the string 'part' from the file name (eg 'Episode Name Part 1' becomes 'Episode Name' (as the 1 was removed above) )
 
-                if default_episode(title) and IGNORE_DEFAULT_EPISODE_NAME:
+                if default_episode(str.lower(title)) and IGNORE_DEFAULT_EPISODE_NAME:
                     # This function tests is the episode title is just 'default' naming. eg 'Episode 1'
                     # In these cases, the user might not want/need 'Episode 1' in the file name.
-                    # What I'd like to do is then test the S01E01 matches, but for now, we'll just test for it with the option to skip these files
 
                     pass
 

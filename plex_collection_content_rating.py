67108d55acbfe2c1601481077b1de68f1dfabab1
David Aldred <extrobe@users.noreply.github.com>
2021-09-18 15:39:04 +1000

    Summary of Changes at the end of the script

--- plex_collection_content_rating.py
+++ plex_collection_content_rating.py
@@ -1,7 +1,7 @@
 from plexapi.server import PlexServer
 
 baseurl = 'http://192.168.1.208:32400'
-token = ' '
+token = ''
 ratings_rank = {
     "gb/E"   : 00,
     "gb/Uc"  : 10,
@@ -15,11 +15,11 @@ ratings_rank = {
     }
 
 DRY_RUN = True
-LIMIT = None
+LIMIT = None # Set to None to scan all collections
 
 plex = PlexServer(baseurl,token)
 collection_list = []
-
+update_list = []
 
 def update_collection_ratings():
     """Scan and update collections"""
@@ -57,11 +57,23 @@ def update_collection_ratings():
                     print("UPDATED")
                 else:
                     print("DRY RUN - NO CHANGES MADE")
+                
+                update_list.append(collection.title)
 
             else:
                 print("NO UPDATE NECESSARY")
 
         collection_list.clear()
+    
+    # Print out summary of what changed / would have been changed
+    print()
+    if not DRY_RUN:
+        print('COLLECTIONS UPDATED:')
+    else:
+        print('COLLECTIONS TO BE UPDATED')
+    for item in update_list:
+        print(item)
+    print()
 
 def main():
     """Kick off the primary process."""
